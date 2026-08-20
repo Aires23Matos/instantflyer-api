99cf77c7900bdca458a6eb3ecc001ea5a781a7bc
Aires23Matos <aires2matos18@gmail.com>
2026-08-20 22:36:59 +0100

    init: mapeando as DTO

---
+++ src/application/mappers/FlyerMapper.ts
@@ -0,0 +1,18 @@
+import { Flyer } from '../../domain/entities/Flyer';
+import { FlyerDTO } from '../dtos/FlyerDTO';
+
+export class FlyerMapper {
+  static toDTO(flyer: Flyer, baseUrl: string): FlyerDTO {
+    return {
+      id: flyer.id!,
+      title: flyer.title,
+      description: flyer.description,
+      fileName: flyer.fileName,
+      fileType: flyer.fileType,
+      notificationDays: flyer.notificationDays,
+      fileUrl: `${baseUrl}/api/flyers/${flyer.id}/file`,
+      createdAt: flyer.createdAt!,
+      updatedAt: flyer.updatedAt!,
+    };
+  }
+}
