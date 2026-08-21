f28b4eb5304f936554cb0928b24883e31e6e36a6
Aires23Matos <aires2matos18@gmail.com>
2026-08-22 00:08:15 +0100

    fix: atualizando password

--- src/infrastructure/database/config/knex.ts
+++ src/infrastructure/database/config/knex.ts
@@ -9,7 +9,7 @@ const dbConfig = {
     host: process.env.DB_HOST || 'mysql.railway.internal',
     port: Number(process.env.DB_PORT || '3306') ,
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'UgQpMCwzfgqQeITuVCCUzZlSWNeEhvCZ',
+    password: process.env.DB_PASSWORD || 'lttTfzjpysirBqaOKvejvXxvaehSxnBH',
     database: process.env.DB_NAME || 'railway',
   },
   pool: { min: 2, max: 10 },
